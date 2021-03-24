c3adc4a514f4eb40ad465578aa8dfadd491d1043
lxsang <xsang.le@gmail.com>
2021-03-24 22:12:50 +0100

    update start entry

--- start.sh
+++ start.sh
@@ -3,5 +3,7 @@
 adduser --home "/home/$ANTOS_USER" --disabled-password --gecos "" "$ANTOS_USER"
 echo -e "$ANTOS_PASSWORD\n$ANTOS_PASSWORD" | passwd "$ANTOS_USER"
 # start antd-tunnel service
+/usr/bin/antd /app/antd-config.ini &
+sleep 1
 [ -f /app/runner.ini ] && /opt/www/bin/runner /app/runner.ini &
-/usr/bin/antd /app/antd-config.ini
+wait
