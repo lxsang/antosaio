f24f09b2d89638d1242f89e61577d5dd7db1228b
lxsang <xsang.le@gmail.com>
2021-03-26 14:41:34 +0100

    refactory app dir

--- start.sh
+++ start.sh
@@ -1,10 +1,12 @@
 #! /bin/sh
-mkdir -p /app/home
+mkdir -p /app/data
 if [ -f "/app/file.fs" ]; then
-    mount /app/file.fs /app/home
-    ls /app
+    mount /app/file.fs /app/data
 fi
-[ ! -e "/home/$ANTOS_USER" ] && ln -sf /app/home "/home/$ANTOS_USER"
+mkdir -p /app/data/home
+mkdir -p /app/data/database
+mkdir -p /app/data/tmp
+[ ! -e "/home/$ANTOS_USER" ] && ln -sf /app/data/home "/home/$ANTOS_USER"
 adduser --home "/home/$ANTOS_USER" --disabled-password --gecos "" "$ANTOS_USER"
 echo -e "$ANTOS_PASSWORD\n$ANTOS_PASSWORD" | passwd "$ANTOS_USER"
 # start antd-tunnel service
