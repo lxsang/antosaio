3594eae109d0eb9e089d652bf974eca5ab949200
lxsang <xsang.le@gmail.com>
2021-03-26 14:03:33 +0100

    support mount loop file

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #! /bin/sh
+mkdir -p /app/home
+if [ -f "/app/file.fs" ]; then
+    mount /app/file.fs /app/home
+    ls /app
+fi
 [ ! -e "/home/$ANTOS_USER" ] && ln -sf /app/home "/home/$ANTOS_USER"
 adduser --home "/home/$ANTOS_USER" --disabled-password --gecos "" "$ANTOS_USER"
 echo -e "$ANTOS_PASSWORD\n$ANTOS_PASSWORD" | passwd "$ANTOS_USER"
